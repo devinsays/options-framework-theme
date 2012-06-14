34c3e702c523d83ba22d94d8d11190c5f90ce6f2
Eli Horn <elihorn@gmail.com>
2012-06-14 20:39:11 +0200

    Swapped out js colorpicker for wordpress native farbtastic

--- inc/options-framework.php
+++ inc/options-framework.php
@@ -180,7 +180,6 @@ if ( !function_exists( 'optionsframework_add_page' ) ) {
 
 function optionsframework_load_styles() {
 	wp_enqueue_style('optionsframework', OPTIONS_FRAMEWORK_DIRECTORY.'css/optionsframework.css');
-	wp_enqueue_style('color-picker', OPTIONS_FRAMEWORK_DIRECTORY.'css/colorpicker.css');
 }	
 
 /* Loads the javascript */
@@ -192,7 +191,7 @@ function optionsframework_load_scripts($hook) {
 	
 	// Enqueued scripts
 	wp_enqueue_script('jquery-ui-core');
-	wp_enqueue_script('color-picker', OPTIONS_FRAMEWORK_DIRECTORY.'js/colorpicker.js', array('jquery'));
+	wp_enqueue_script( 'farbtastic' );
 	wp_enqueue_script('options-custom', OPTIONS_FRAMEWORK_DIRECTORY.'js/options-custom.js', array('jquery'));
 	
 	// Inline scripts from options-interface.php

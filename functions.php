ed2e981dc8f2f90759e492e204857ce7fe45eccb
Vinod Dalvi <mozillavvd@gmail.com>
2013-09-29 01:11:34 -0700

    Improved coding standards.

--- functions.php
+++ functions.php
@@ -1,25 +1,25 @@
 <?php
 
-/* 
+/*
  * Loads the Options Panel
  *
  * If you're loading from a child theme use stylesheet_directory
  * instead of template_directory
  */
- 
-if ( !function_exists( 'optionsframework_init' ) ) {
+
+if ( ! function_exists( 'optionsframework_init' ) ) {
 	define( 'OPTIONS_FRAMEWORK_DIRECTORY', get_template_directory_uri() . '/inc/' );
 	require_once dirname( __FILE__ ) . '/inc/options-framework.php';
 }
 
-/* 
+/*
  * This is an example of how to add custom scripts to the options panel.
  * This one shows/hides the an option when a checkbox is clicked.
  *
  * You can delete it if you not using that option
  */
 
-add_action('optionsframework_custom_scripts', 'optionsframework_custom_scripts');
+add_action( 'optionsframework_custom_scripts', 'optionsframework_custom_scripts' );
 
 function optionsframework_custom_scripts() { ?>
 
@@ -29,13 +29,13 @@ jQuery(document).ready(function() {
 	jQuery('#example_showhidden').click(function() {
   		jQuery('#section-example_text_hidden').fadeToggle(400);
 	});
-	
+
 	if (jQuery('#example_showhidden:checked').val() !== undefined) {
 		jQuery('#section-example_text_hidden').show();
 	}
-	
+
 });
 </script>
- 
+
 <?php
 }
